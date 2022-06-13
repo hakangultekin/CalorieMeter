fdb44f28e09c85d7cbd793d29bf210f74554c93a
Asena Sevnur <you@example.com>
2022-06-13 18:14:35 +0300

    sa eklendi

--- CaloriMeter.Model/Entitites/Food.cs
+++ CaloriMeter.Model/Entitites/Food.cs
@@ -26,7 +26,7 @@ namespace CaloriMeter.Model.Entitites
         public virtual User User { get; set; }
         public virtual Category Category { get; set; }
         public virtual ICollection<MealDetail> MealDetails { get; set; }
-
+        //sa
         // Son olarak
     }
 }
