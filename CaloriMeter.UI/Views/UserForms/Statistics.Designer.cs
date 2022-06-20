10355db2178b6fa727a05492d5f1a60a6ec21dec
Hakan Gültekin <hakan35@hotmail.it>
2022-06-20 15:26:17 +0300

    ufak duzeltme

--- CaloriMeter.UI/Views/UserForms/Statistics.Designer.cs
+++ CaloriMeter.UI/Views/UserForms/Statistics.Designer.cs
@@ -612,7 +612,6 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label6);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximumSize = new System.Drawing.Size(762, 883);
             this.Name = "Statistics";
